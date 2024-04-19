1ac09facbed872bb0d1857f5060a6788ed12d48d
fanchThib <fanch.thib@gmail.com>
2024-04-19 10:49:28 +0200

    component init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { PresentationComponent } from './component/presentation/presentation.component';
+import { AboutComponent } from './component/about/about.component';
+import { NetworkComponent } from './component/network/network.component';
+import { HistoryComponent } from './component/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioComponent,
+    PresentationComponent,
+    AboutComponent,
+    NetworkComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
